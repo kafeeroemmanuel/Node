7b47fb86484403757b55bd3e6b8da46d406d563f
kafeeroemmanuel <emzpipi.com>
2023-07-11 18:59:27 +0300

    [feat] - Switching to sequelize.

--- util/database.js
+++ util/database.js
@@ -1,14 +1,23 @@
-//connecting our app to the db
-const mysql = require("mysql2");
+// //connecting our app to the db
+// const mysql = require("mysql2");
 
-//create pool gives a bunch of connections where many queries can be run instead of create a solo connextion every time
-const pool = mysql.createPool({
+// //create pool gives a bunch of connections where many queries can be run instead of create a solo connextion every time
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "#Mbatudde",
+// });
+
+// module.exports = pool.promise();
+
+// //dive into official docs to learn more about mysql
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "#Mbatudde", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "#Mbatudde",
 });
 
-module.exports = pool.promise();
-
-//dive into official docs to learn more about mysql
+module.exports = sequelize;
